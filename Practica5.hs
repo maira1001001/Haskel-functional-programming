ca4736c501f15544ab19c8012d050dc03e46e873
Maira Diaz <mairadiaz@protonmail.ch>
2018-12-15 19:35:06 -0300

    algunos ejercicios de la practica5

---
+++ Practica5.hs
@@ -0,0 +1,54 @@
+--practica 5
+--tipos algebraicos
+--pattern matching
+--LISTAS:
+---- se define inductivamente al conjunto [a] con las siguientes reglas:
+---- 1. [] :: [a]
+---- 2. si x :: a y xs :: [a] entonces x:xs :: [a]
+
+import Data.Char(ord)
+
+--sum: suma los elementos de una lista de números. 
+sum' []     = 0
+sum' (x:xs) = x + sum xs
+
+--any: devuelve True si algún elemento de una lista es True.
+any' []     = False
+any' (x:xs) = x || any' xs 
+
+--allTrue: describe verdadero si todos los elementos de la lista son True.
+allTrue []     = True
+allTrue (x:xs) = x && allTrue xs
+
+--allFalse: describe verdadero si todos los elementos de la lista describen Falso
+allFalse []     = True
+allFalse (x:xs) = not x && allFalse xs
+
+--anyFalse : describe Verdadero si algun elemento de la lista es False
+anyFalse = not.allTrue
+
+--anyTrue: describe verdadero si algún elemento de la lista es True
+anyTrue = not.allFalse
+
+--codes: dada una lista de caracteres, devuelve la lista de sus códigos.
+codes :: [Char] -> [Int]
+codes []     = []
+codes (c:cs) = ord c : codes cs
+
+--remainders:  dada una lista de números, devuelve los restos de su división por un número
+remainders :: Int -> [Int] -> [Int]
+remainders d []     = []
+remainders d (n:ns) = mod n d : remainders d ns
+
+--squares: dada una lista de números, devuelve la lista de sus cuadrados.
+squares :: [Int] -> [Int]
+squares []      = []
+squares (n:ns)  = n*n : squares ns
+
+--lengths: dada una lista de listas, devuelve la lista de sus longitudes.
+lengths :: [[a]] -> [Int]
+lengths []       = 0
+lengths (xs:xss) = lenl xs : lengths xss
+
+lenl [] = 0
+lenl (x:xs) = 1 + lenl xs
