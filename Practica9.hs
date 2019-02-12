61cf5cab34f687aa796f2cba3460382c4d418aba
maii <maira.diaz@protonmail.ch>
2019-02-12 11:55:03 -0300

    ejercicio 2

--- Practica9.hs
+++ Practica9.hs
@@ -1,5 +1,6 @@
 --Práctica 9.  Temas: PAtrones genéricos de recursión. Funciones sobre árboles.
 
+--Ejercicio 1
 data TipTree a = Tip a | Join (TipTree a) (TipTree a) deriving Show
 
 foldTip :: (a->b)->(b -> b -> b) -> TipTree a -> b
@@ -18,7 +19,26 @@ nodes = foldTip (const 0) (\n1 n2 -> 1 + n1 + n2)
 
 walkover = foldTip (\h -> [h]) (\xs1 xs2 -> xs1 ++ xs2)
 
- mapTip f = foldTip (\x -> Tip (f x)) (\t1 t2 -> Join t1 t2)
+mapTip f = foldTip (\x -> Tip (f x)) (\t1 t2 -> Join t1 t2)
 
 tipTreeExample1 = Join (Tip 1) (Join (Tip 2) (Tip 3))
 pot = \x->x*x
+
+
+--Ejercicio 2
+data BinTree a = Empty | Bin a (BinTree a) (BinTree a) deriving Show
+
+foldBin :: (a -> b -> b -> b) -> b -> BinTree a -> b
+foldBin f z Empty = z
+foldBin f z (Bin x t1 t2) = f x (foldBin f z t1) (foldBin f z t2)
+
+nodesBin = foldBin (\x n1 n2 -> 1 + n1 + n2) 0
+
+heightBin = foldBin (\x h1 h2 -> 1 + max h1 h2) 0
+
+--mapBin f = foldBin (\x b1 b2 -> Bin (f x) b1 b2) id
+
+--mirrorBin = foldBin (\x b1 b2 -> Bin x b2 b1) id
+
+binTreeExample = Bin 1 Empty (Bin 2 Empty (Bin 3 Empty Empty))
+
